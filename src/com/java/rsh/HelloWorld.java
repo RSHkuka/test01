8106297f4722b3ca0eaa098902810d77798c549c
RSHkuka <571473250@qq.com>
2022-03-01 14:12:38 +0800

    更新测试

--- src/com/java/rsh/HelloWorld.java
+++ src/com/java/rsh/HelloWorld.java
@@ -3,5 +3,13 @@ package com.java.rsh;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("你好");
+        test();
+    }
+
+    /*
+     *
+     */
+    public static void test() {
+        System.out.println("Thanks");
     }
 }

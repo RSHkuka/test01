c2e33901d59ec2e24c6b42f99c4e6340692a1f41
RSHkuka <571473250@qq.com>
2022-03-01 14:16:32 +0800

    back测试

--- src/com/java/rsh/HelloWorld.java
+++ src/com/java/rsh/HelloWorld.java
@@ -4,6 +4,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("你好");
         test();
+        System.out.println("I will come back");
     }
 
     /*
